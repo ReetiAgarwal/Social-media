c8391ec2d60776c2c3c437550458a69288dd9835
ReetiAgarwal <87187281+ReetiAgarwal@users.noreply.github.com>
2024-02-28 16:30:56 +0530

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,24 +2,7 @@ import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-    user : {
-        _id : "658fe855633ab8e86278715f",
-        username : "Geeta",
-        email : "kjr@gmail.com",
-        password : "$2b$10$jZTdIFhwv3M4xKZdRzqQLOR8Igv6x9QbObrah9KhIAMG6tZIcv5LK",
-        profilePicture : "/images/geeta.jpg",
-        coverPicture : "",
-        followers : [],
-        followings : ["658eb8195344b2cff10ad424"],
-        isAdmin : false,
-        createdAt : {"$date":{"$numberLong":"1703929941596"}},
-        updatedAt : {"$date":{"$numberLong":"1704355803860"}},
-        city : "Noida",
-        desc : "Hello Everyone!",
-        from : "Sector-62",
-        relationship : "Single"
-    },
-    // user : null,
+    user : null,
     isFetching : false,
     error : false,
 };
